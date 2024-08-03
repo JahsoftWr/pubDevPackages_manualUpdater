5e6cc390b6c3b35602e9ff8d9bf354d1f864187e
Javier <jahsoftw@gmail.com>
2024-08-03 19:12:33 -0300

    Primer commit: Archivos iniciales del proyecto

---
+++ README.md
@@ -0,0 +1,1 @@
+# pubDevPackages_manualUpdater
---
+++ version.php
@@ -0,0 +1,30 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $packageName = $_POST['package'];
+
+    // URL de la API de pub.dev
+    $apiUrl = 'https://pub.dev/api/packages/' . $packageName;
+
+    // Utilizar cURL para realizar la solicitud HTTP
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $apiUrl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    // Decodificar la respuesta JSON
+    $data = json_decode($response, true);
+
+    // Manejar posibles errores en la respuesta
+    if (isset($data['error'])) {
+        echo json_encode(['error' => $data['error']['message']]);
+    } else {
+        // Obtener la versión
+        $version = $data['latest']['version'] ?? 'No se encontró la versión';
+
+        echo json_encode(['version' => $version]);
+    }
+} else {
+    // Manejar caso de acceso directo (opcional)
+    echo "Acceso no válido. Utiliza el formulario.";
+}
